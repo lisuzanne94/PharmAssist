9e55d3f4efc003f2826e729f3d6bf22a548efcaa
Suzi <lisuzanne94@gmail.com>
2021-11-14 17:43:01 -0500

    set up skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require("./routes/api/users");
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World! :D"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
