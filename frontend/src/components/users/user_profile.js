26df7d914d4a3e9c5d28a23f752ace42fe5276fb
Subin Cho <subinee1633@gmail.com>
2021-11-15 11:46:32 -0500

    add user profile component

---
+++ frontend/src/components/users/user_profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            <section className='user-nav'>
+            
+            </section>
+
+            <section className='user-medication-list'>
+
+            </section>
+
+            <section className='user-calendar'>
+
+            </section>
+
+            <section className='user-pharmacy-map'>
+
+            </section>
+        </div>
+    )
+}
+
+export default UserProfile;
---
+++ frontend/src/components/users/user_profile_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import UserProfile from './user_profile';
+
+const mSTP = state => ({
+    
+});
+
+const mDTP = state => ({
+
+});
+
+export default connect(mSTP, mDTP)(UserProfile)
