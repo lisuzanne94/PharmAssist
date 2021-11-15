89292de03eded4a625e478fc8d8295d089eab039
spark <spark11491@gmail.com>
2021-11-15 14:09:59 -0500

    create medications validation

--- validation/medications.js
+++ validation/medications.js
@@ -2,3 +2,27 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 const validNumber = require('./valid-number');
 
+module.exports = function validateMedicationInput(data) {
+    let errors = {};
+
+    data.brandName = validText(data.brandName) ? data.brandName : '';
+    data.dose = validNumber(data.dose) ? data.dose : '';
+    data.frequency = validNumber(data.frequency) ? data.frequency : '';
+
+    if (Validator.isEmpty(data.brandName)) {
+        errors.brandName = 'Brand name field is required';
+    }
+
+    if (Validator.isEmpty(data.dose)) {
+        errors.dose = 'Dosage field is required';
+    }
+
+    if (Validator.isEmpty(data.frequency)) {
+        errors.frequency = 'Frequency field is required';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
+};
