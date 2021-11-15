432ec4dadff4d973e1608f6ed5165fba5da05695
Suzi <lisuzanne94@gmail.com>
2021-11-15 14:09:11 -0500

    add basic formatting skeleton for splash

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -3,10 +3,27 @@ import React from 'react';
 const Splash = () => {
 
     return (
-        <div>
+        <div className="splash-page-container">
             <h1>PharmAssist</h1>
+
+            <div className="splash-about">
+                THIS IS THE ABOUT
+            </div>
+
+            <div className="splash-med-list">
+                THIS IS THE MED LIST
+            </div>
+
+            <div className="splash-calendar">
+                THIS IS THE CALENDAR
+            </div>
+
+            <div className="splash-map">
+                THIS IS THE MAP
+            </div>
+
             <footer>
-                Copyright &copy; 2021 Susanne Li, Subin Cho, Sohee Park, Matt Swedin
+                Copyright &copy; 2021 Suzanne Li, Subin Cho, Sohee Park, Matt Swedin
             </footer>
         </div>
     );
