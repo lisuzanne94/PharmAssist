d7c0d81931583c9997c573cab9cd06d425eea7a7
Subin Cho <subinee1633@gmail.com>
2021-11-15 20:48:32 -0500

    style user profile page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import Splash from './splash/splash';
+import UserProfile from './users/user_profile';
 
 import NavBarContainer from './nav/navbar_container';
 import LoginFormContainer from './session/login_form_container';
@@ -9,11 +10,13 @@ import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div>
-        <NavBarContainer />
+        {/* <NavBarContainer /> */}
         <Switch>
             <AuthRoute exact path='/' component={Splash} />
             <AuthRoute exact path='/login' component={LoginFormContainer} />
             <AuthRoute exact path='/signup' component={SignupFormContainer} />
+        
+            <ProtectedRoute exact path='/main' component={UserProfile} />
         </Switch>
     </div>
 );
--- frontend/src/components/users/user_profile.js
+++ frontend/src/components/users/user_profile.js
@@ -3,32 +3,41 @@ import { Link } from 'react-router-dom';
 
 const UserProfile = () => {
     return (
-        <div>
+        <div id='user-profile'>
             <section id='user-nav'>
-                <div className='user-profile-picture'>
-                    <img src='' alt='user profile picture' />
+                <div className='user-nav-header'>
+                    <h2 className='pharmassist-header'>
+                        <span>Pharm</span><span className='assist'>Assist</span>
+                    </h2>
+                    <div className='user-nav-divider'>
+                    </div>
                 </div>
-
                 <div className='user-nav-links'>
-                    <Link to=''>List of Medications</Link>
-                    <Link to=''>Calendar</Link>
-                    <Link to=''>Find Your Nearest Pharmacy</Link>
+                    <Link to=''>Medication List</Link><br/>
+                    <Link to=''>Calendar</Link><br/>
+                    <Link to=''>Find Your Nearest Pharmacy</Link><br/>
                 </div>
             </section>
 
-            <section className='user-medication-list'>
-                <ul>
-                    <li></li>
-                </ul>
-            </section>
+            <div id='section-right'>
+                <section id='user-medication-list'>
+                    <h1 className='user-medication-list-header'>Medication List</h1>
+                    <ul>
+                        <li className='user-medication-list-item'>Amlodipine 10mg</li>
+                        <li className='user-medication-list-item'>Carvedilol 12.5mg</li>
+                        <li className='user-medication-list-item'>Amoxicillin 500mg</li>
+                        <li className='user-medication-list-item'>Sertraline 50mg</li>
+                    </ul>
+                </section>
 
-            <section className='user-calendar'>
+                <section id='user-calendar'>
+                    <h1 className='user-calendar-header'>Calendar</h1>
+                </section>
 
-            </section>
-
-            <section className='user-pharmacy-map'>
-
-            </section>
+                <section id='user-pharmacy-map'>
+                    <h1 className='user-pharmacy-map-header'>Find a Pharmacy</h1>
+                </section>
+            </div>
         </div>
     )
 }
