6925cd5e4e21b1825002ccf38fc569045cb4209a
Suzi <lisuzanne94@gmail.com>
2021-11-17 00:03:44 -0500

    deploy to heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const medications = require('./routes/api/medications');
@@ -24,3 +25,10 @@ app.use("/api/medications", medications);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
