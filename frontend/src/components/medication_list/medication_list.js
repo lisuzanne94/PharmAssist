39f57f2e97a711bd95af60bdb9b17716cae04491
Suzi <lisuzanne94@gmail.com>
2021-11-17 19:49:48 -0500

    style med list

--- frontend/src/components/medication_list/medication_list.js
+++ frontend/src/components/medication_list/medication_list.js
@@ -1,18 +1,29 @@
 import React, { useEffect } from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const MedicationList = ({ medications, deleteMedication, openModal }) => {
 
 
     return (
         <div>
-            <ul>
+            <ul className="med-list">
                 {
                     medications ? Object.values(medications).map((medication, i) => (
-                        <li key={i}>
-                            <span onClick={() => openModal({ type: 'getDrugInfo', medication: medication })}>{medication.brandName}</span> {medication.strength}mg
-                            <input type="submit" value="Update" onClick={() => openModal({type: 'updateMedication', medication: medication})} />
-                            <input type="submit" value="Delete" onClick={() => deleteMedication(medication._id) } />
+                        <li key={i} className="med-list-item">
+                            <span className="med-list-name">
+                                <span onClick={() => openModal({ type: 'getDrugInfo', medication: medication })}>{medication.brandName}</span> {medication.strength}mg&nbsp;
+                            </span>
+                            <span className="med-list-sig">{medication.dose === "1" ? "1 time" : `${medication.dose} times`} a day</span>
+                            &nbsp;&nbsp;
+                            <span className="med-list-btns">
+                                <button onClick={() => openModal({type: 'updateMedication', medication: medication})}>
+                                    <FontAwesomeIcon icon={faPen} /> Update
+                                </button>
+                                <button onClick={() => deleteMedication(medication._id) }>
+                                    <FontAwesomeIcon icon={faTrash} /> Delete
+                                </button>
+                            </span>
                         </li>
                     )) : null
                 }
