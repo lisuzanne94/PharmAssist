89b36591e6273f4e0a24df5b3f689e9a8f2de67d
Subin Cho <78716708+subinc1633@users.noreply.github.com>
2021-11-18 11:29:24 -0500

    edit form and about us

--- frontend/src/components/about/about_us.js
+++ frontend/src/components/about/about_us.js
@@ -5,29 +5,33 @@ const AboutUs = ({ classState }) => {
         <div className={classState}>
             <div className='about-member'>
                 <img src=''/>
-                <h2 className='member-name'>Suzanne Li</h2>
-                <div>
+                <h2>Suzanne Li</h2>
+                <h4>Team Lead / Flex</h4>
+                <div className='about-links'>
                     <a href='https://github.com/lisuzanne94'>GitHub</a> | <a href='https://www.linkedin.com/in/suzanne-li-080036161/'>LinkedIn</a>
                 </div>
             </div>
             <div className='about-member'>
                 <img src=''/>
-                <h2 className='member-name'>Subin Cho</h2>
-                <div>
+                <h2>Subin Cho</h2>
+                <h4>Frontend</h4>
+                <div className='about-links'>
                     <a href='https://github.com/subinc1633'>GitHub</a> | <a href='https://www.linkedin.com/in/sc1633/'>LinkedIn</a>
                 </div>
             </div>
             <div className='about-member'>
                 <img src=''/>
-                <h2 className='member-name'>Sohee Park</h2>
-                <div>
+                <h2>Sohee Park</h2>
+                <h4>Backend</h4>
+                <div className='about-links'>
                     <a href='https://github.com/parks14'>GitHub</a> | <a href='https://www.linkedin.com/in/sohee-park-b8164a112/'>LinkedIn</a>
                 </div>
             </div>
             <div className='about-member'>
                 <img src=''/>
-                <h2 className='member-name'>Matt Swedin</h2>
-                <div>
+                <h2>Matt Swedin</h2>
+                <h4>Flex</h4>
+                <div className='about-links'>
                     <a href='https://github.com/mattswedin'>GitHub</a> | <a href='https://www.linkedin.com/in/mattswedin/'>LinkedIn</a>
                 </div>
             </div>
