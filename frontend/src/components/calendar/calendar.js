21aa6948e95f7f5479a659c20f46e06e3845f99b
Suzi <lisuzanne94@gmail.com>
2021-11-17 22:37:00 -0500

    hide scrollbar

--- frontend/src/components/calendar/calendar.js
+++ frontend/src/components/calendar/calendar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCapsules } from '@fortawesome/free-solid-svg-icons'
 
 class Calendar extends React.Component {
   constructor(props) {
