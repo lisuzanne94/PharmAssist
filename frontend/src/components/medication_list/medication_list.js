cd68d480ad1ed386f156dbf430e20c548491ba0c
Suzi <lisuzanne94@gmail.com>
2021-11-18 02:22:17 -0500

    add more styling to calendar

--- frontend/src/components/medication_list/medication_list.js
+++ frontend/src/components/medication_list/medication_list.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen, faTrash, faCapsules } from '@fortawesome/free-solid-svg-icons'
 
 const MedicationList = ({ medications, deleteMedication, openModal }) => {
-
 
     return (
         <div>
@@ -12,20 +10,21 @@ const MedicationList = ({ medications, deleteMedication, openModal }) => {
                     medications ? Object.values(medications).map((medication, i) => (
                         <li key={i} className="med-list-item">
                             <div className="med-list-text" onClick={() => openModal({ type: 'getDrugInfo', medication: medication })}>
-                                <FontAwesomeIcon icon={faCapsules} className="pill-icon" />&nbsp;
                                 <span className="med-list-name">
                                     <span>{medication.brandName}</span> {medication.strength}mg&nbsp;
                                 </span>
-                                <span className="med-list-sig">{medication.frequency === "1" ? "1 time" : `${medication.frequency} times`} a day</span>
+                                <span className="med-list-sig">
+                                    {medication.frequency === "1" ? "1 time" : `${medication.frequency} times`} a day {!medication.duration ? '' : `for ${medication.duration} days`}
+                                </span>
                                 &nbsp;&nbsp;&nbsp;&nbsp;
                             </div>
                             <div className="med-list-btns">
                                 <button onClick={() => openModal({type: 'updateMedication', medication: medication})}>
-                                    <FontAwesomeIcon icon={faPen} /> Update
+                                    Update
                                 </button>
                                 &nbsp;
                                 <button onClick={() => deleteMedication(medication._id) }>
-                                    <FontAwesomeIcon icon={faTrash} /> Delete
+                                    Delete
                                 </button>
                             </div>
                         </li>
