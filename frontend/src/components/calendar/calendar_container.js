c25b346630a8fa601d1b5f6f44d2c4a62b33e94f
Matt Swedin <swedin.matthew@gmail.com>
2021-11-29 14:41:31 -0500

    all links change color and the description modal appears on calendar

--- frontend/src/components/calendar/calendar_container.js
+++ frontend/src/components/calendar/calendar_container.js
@@ -1,12 +1,13 @@
 import { connect } from "react-redux";
 import Calendar from "./calendar";
+import { openModal } from "../../actions/modal_actions";
 
 const mSTP = state => ({
     medications: Object.values(state.entities.medications)
 });
 
 const mDTP = dispatch => ({
-
+    openModal: modal => dispatch(openModal(modal))
 });
 
 export default connect(mSTP, mDTP)(Calendar);
